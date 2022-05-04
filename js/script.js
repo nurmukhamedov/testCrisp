4d23f46797060871705d0add397b2777312e619c
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-05-04 23:56:38 +0500

    grid added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const navMenuList = document.querySelectorAll(".nav__ul-li");
 const removeBtn = document.querySelector(".nav__remove-btn");
 const body = document.body;
 
-
 const animatedList = () => {
     navMenuList.forEach((link, index) => {
         if (link.style.animation) {
@@ -27,3 +26,38 @@ removeBtn.addEventListener("click", () => {
     navMenu.classList.remove("nav__ul-active");
     animatedList();
 });
+
+function change1() {
+    document.getElementById('picture1').src = 'image/product1.svg';
+    document.getElementById('picture2').src = 'image/product2.svg';
+    document.getElementById('picture3').src = 'image/product3.svg';
+    document.getElementById('picture4').src = 'image/product3.svg';
+    document.getElementById('picture5').src = 'image/product1.svg';
+}
+
+function change2() {
+    document.getElementById('picture1').src = 'image/product1-blue.svg';
+    document.getElementById('picture2').src = 'image/product2-blue.svg';
+    document.getElementById('picture3').src = 'image/product3-blue.svg';
+    document.getElementById('picture4').src = 'image/product3-blue.svg';
+    document.getElementById('picture5').src = 'image/product1-blue.svg';
+
+}
+
+function change3() {
+    document.getElementById('picture1').src = 'image/product1-purple.svg';
+    document.getElementById('picture2').src = 'image/product2-purple.svg';
+    document.getElementById('picture3').src = 'image/product3-purple.svg';
+    document.getElementById('picture4').src = 'image/product3-purple.svg';
+    document.getElementById('picture5').src = 'image/product1-purple.svg';
+
+}
+
+function change4() {
+    document.getElementById('picture1').src = 'image/product1-gray.svg';
+    document.getElementById('picture2').src = 'image/product2-gray.svg';
+    document.getElementById('picture3').src = 'image/product3-gray.svg';
+    document.getElementById('picture4').src = 'image/product3-gray.svg';
+    document.getElementById('picture5').src = 'image/product1-gray.svg';
+
+}
